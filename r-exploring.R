b862919a122c0a3e8155a64b7e65271a3cd9ea04
ht-oliver <holiver327@gmail.com>
2025-08-07 13:42:55 -0700

    histogram

--- r-exploring.R
+++ r-exploring.R
@@ -4,10 +4,10 @@ library(skimr)
 library(palmerpenguins)
 library(tidyverse)
 
-#Look at it
+# Look at it
 view(penguins)
 
-#Check the column names
+# Check the column names
 names(penguins)
 
 # Check the dimensions
@@ -16,5 +16,25 @@ dim(penguins)
 # Get a summary
 summary(penguins)
 
-# Geta summary with {skimr}
+# Get a summary with {skimr}
 skimr::skim(penguins)
+
+# Print the first 6 rows
+head(penguins)
+
+# Print the last 6 rows
+tail(penguins)
+
+# Make a pairplot (sor of overwhelming with all data columns)
+GGally::ggpairs(penguins)
+
+# Make a pairsplot with a select number of colums and color by species
+GGally::ggpairs(penguins, 
+                columns = 3:6,
+                ggplot2::aes(colour = species))
+
+# Make a histogram of penguin flipper lengths 
+ggplot(data = penguins, aes(x = flipper_length_mm)) + 
+  geom_histogram()
+
+
